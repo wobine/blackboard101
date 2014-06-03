9aea5711c0381e2a9ea830d628fbdd41e71d73d8
James D'Angelo <sputknick@gmail.com>
2014-06-03 11:34:19 -0400

    Update EllipticCurvesPart4-PrivateKeyToPublicKey.py

--- EllipticCurvesPart4-PrivateKeyToPublicKey.py
+++ EllipticCurvesPart4-PrivateKeyToPublicKey.py
@@ -8,7 +8,7 @@ N=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 # Number of
 Acurve = 0; Bcurve = 7 # These two defines the elliptic curve. y^2 = x^3 + Acurve * x + Bcurve
 Gx = 55066263022277343669578718895168534326250603453777594175500187360389116729240
 Gy = 32670510020758816978083085130507043184471273380659243275938904335757337482424
-GPoint = (Gx,Gy) # This is our generator point. Tillions of dif ones possible
+GPoint = (Gx,Gy) # This is our generator point. Trillions of dif ones possible
 
 #Individual Transaction/Personal Information
 privKey = 0xA0DC65FFCA799873CBEA0AC274015B9526505DAAAED385155425F7337704883E #replace with any private key
